2bd40254ee558e2c82e7753c4c5b6810981f2519
s-b-m <amajor2@mail.csuchico.edu>
2019-11-18 11:49:43 -0800

    Updated test suite for mostPopularDigit

--- randoTest.cpp
+++ randoTest.cpp
@@ -20,7 +20,7 @@ class RandoTest : public ::testing::Test
 	protected:
 		RandoTest(){} //constructor runs before each test
 		virtual ~RandoTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
+	 	virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
@@ -30,7 +30,37 @@ TEST(RandoTest, sanityCheck)
 	ASSERT_TRUE( true );
 }
 
-TEST(RandoTest, thisWillFail)
+TEST(RandoTest, MPDZero)
 {
-	ASSERT_TRUE( false );
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit(0), "0");
+}
+
+TEST(RandoTest, MPDTwoWayTie)
+{
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit(331311), "-1")
+}
+
+TEST(RandoTest, MPDTenWayTie)
+{
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit(1234567890), "-1")
+}
+
+TEST(RandoTest, MPDOneThreeTimes)
+{
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit("11331"), "1")
+}
+
+TEST(RandoTest, MPDOnlyTwos)
+{
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit(222), "2")
+}
+
+TEST(RandoTest, MPDCharacter)
+  Rando rando;
+  ASSERT_EQ(rando.mostPopularDigit(64), "-1")
 }
